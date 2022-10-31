fee18c7a77f195031ca5c855b58965e3fad63f5b
Gerardo Ramírez <gedrix.rm93@gmail.com>
2022-10-30 20:12:20 -0500

    finalizacion de maquetacion

--- peli_app/lib/screeens/details_screen.dart
+++ peli_app/lib/screeens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peli_app/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
    
@@ -14,6 +15,21 @@ class DetailsScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           _CustomAppBar(),
+
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndtitle(),
+              
+              _OverView(),
+              _OverView(),
+              _OverView(),
+              _OverView(),
+              _OverView(),
+
+              CastingCards(),
+            ]
+            )
+          )
         ],
       )
     );
@@ -36,8 +52,9 @@ class _CustomAppBar extends StatelessWidget {
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           color: Colors.black12,
+          padding: EdgeInsets.only(bottom: 10),
           child: const Text(
-            'Movie',
+            'movie.title',
             style: TextStyle(fontSize: 16),
             )
         ),
@@ -47,6 +64,65 @@ class _CustomAppBar extends StatelessWidget {
             fit:BoxFit.cover
           )
       ),
+    );
+  }
+}
+
+
+class _PosterAndtitle  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final TextTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/200x300'),
+            height: 150,
+            ),
+          ),
+          SizedBox(width: 20),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title',style: TextTheme.headline5, overflow:  TextOverflow.ellipsis, maxLines: 2,),
+              Text('movie.originalTitle',style: TextTheme.subtitle1, overflow:  TextOverflow.ellipsis, maxLines: 1,),
+              Row(
+                 children: [
+                  Icon(Icons.star_outlined, size:15, color: Colors.grey),
+                  SizedBox(width: 5),
+                  Text('movie.voteAverage', style: TextTheme.caption)
+                 ],
+              )
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+
+class _OverView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   final TextTheme = Theme.of(context).textTheme;
+    
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text('Tempor ex aute amet ullamco laboris culpa laboris irure velit do qui pariatur laborum reprehenderit. Aute dolore nulla ad enim esse et eiusmod. Mollit ea ad dolore culpa occaecat occaecat irure consectetur ullamco voluptate. Mollit duis fugiat esse id fugiat.',
+       textAlign: TextAlign.justify,
+        style: TextTheme.subtitle1),
     );
   }
 }
--- peli_app/lib/widgets/widgets.dart
+++ peli_app/lib/widgets/widgets.dart
@@ -1,5 +1,4 @@
 
-export 'package:peli_app/widgets/movie_slider.dart';
-
 export 'package:peli_app/widgets/card_swiper.dart';
-
+export 'package:peli_app/widgets/casting_cards.dart';
+export 'package:peli_app/widgets/movie_slider.dart';
